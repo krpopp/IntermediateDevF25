4ab1e0d0c3e818fe56ebdabb40f67968c7b3c913
krpopp <kp1640@nyu.edu>
2025-09-10 10:39:48 -0400

    adding comments to week 1 code

--- Assets/Scripts/BloomFlower.cs
+++ Assets/Scripts/BloomFlower.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class BloomFlower : MonoBehaviour
 {
+    //'public' sets the scope level of a variable, method, or class to be accesible to all other classes
     public bool didDing = false;
 }
