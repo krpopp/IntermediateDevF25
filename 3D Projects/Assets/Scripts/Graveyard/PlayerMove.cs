00f360bfce3497f4468f775ea1565784f20428ab
krpopp <kp1640@nyu.edu>
2025-10-27 19:42:16 -0400

    adding 3d graveyard rotation control

--- 3D Projects/Assets/Scripts/Graveyard/PlayerMove.cs
+++ 3D Projects/Assets/Scripts/Graveyard/PlayerMove.cs
@@ -9,16 +9,32 @@ public class PlayerMove : MonoBehaviour
 
     Camera cam;
 
+    [SerializeField]
+    float rotationSmooth;
+
+    float currentAngle, currentVel;
+
+    CharacterController charController;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
-        transform.position += movementInput * speed * Time.deltaTime;
+        if(movementInput.magnitude >= 0.1f)
+        {
+            float target = Mathf.Atan2(movementInput.x, movementInput.z) * Mathf.Rad2Deg + cam.transform.eulerAngles.y;
+            currentAngle = Mathf.SmoothDampAngle(currentAngle, target, ref currentAngle, rotationSmooth);
+            transform.rotation = Quaternion.Euler(0, currentAngle, 0);
+            Vector3 rotateMove = Quaternion.Euler(0, target, 0) * Vector3.forward;
+            charController.Move(rotateMove * speed * Time.deltaTime);
+        }
+        //transform.position += movementInput * speed * Time.deltaTime;
     }
 }
